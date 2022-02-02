050baeed6a3141122f96bff63ffa92913b58773d
cayde.roothoff <cayde.roothoff@route.com>
2022-02-01 17:18:23 -0700

    added ability to include jokers

--- deck.go
+++ deck.go
@@ -15,22 +15,31 @@ func main(){
 	shuffleDeck(deck)
 }
 
-func createDeck() []Card{
+func createDeck(numJokers int) []Card{
 	var deck []Card
 	for i := 1; i < 5; i++{
 		for j := 1; j < 14; j++{
 			var currCard = Card{}
 			if i == 1{
-				currCard.suit = "c"
+				currCard.suit = "s"
 			} else if i == 2{
 				currCard.suit = "d"
 			} else if i == 3{
-				currCard.suit = "h"
+				currCard.suit = "c"
 			} else if i == 4{
-				currCard.suit = "s"
+				currCard.suit = "h"
 			}
 			currCard.number = j
 			deck = append(deck, currCard)
+		}
+	}
+	if numJokers > 0{
+		for i := 0; i < numJokers; i++{
+			joker := Card{
+				number: 0,
+				suit: "j",
+			}
+			deck = append(deck, joker)
 		}
 	}
 	return deck
--- deck_test.go
+++ deck_test.go
@@ -6,17 +6,22 @@ import (
 )
 
 func Test_createDeck(t *testing.T) {
-	deck := createDeck()
+	deck := createDeck(0)
 	if len(deck) != 52{
+		t.Errorf("Deck not created correctly")
+	}
+
+	deckWithJokers := createDeck(2)
+	if len(deckWithJokers) != 54{
 		t.Errorf("Deck not created correctly")
 	}
 }
 
 func Test_shuffleDeck(t *testing.T) {
-	shuffledDeck := createDeck()
+	shuffledDeck := createDeck(0)
 	shuffleDeck(shuffledDeck)
 
-	origDeck := createDeck()
+	origDeck := createDeck(0)
 
 	if reflect.DeepEqual(origDeck, shuffledDeck){
 		t.Errorf("Deck was not shuffled")
@@ -24,8 +29,7 @@ func Test_shuffleDeck(t *testing.T) {
 }
 
 func Test_sort(t *testing.T) {
-	deck := createDeck()
+	deck := createDeck(0)
 	sortDeck(deck, 4, 52/4 - 4, true)
 
-	if
 }
